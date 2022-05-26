9ff454c40a68ca8e21ff76edb64f001deab0c82f
Javier Méndez <javiermendez0299@gmail.com>
2022-05-26 09:24:39 -0700

    Initial commit

---
+++ src/pages/QuoteDetail.js
@@ -0,0 +1,40 @@
+import { Fragment } from "react";
+import { Route, useParams, Link, useRouteMatch } from "react-router-dom";
+
+import Comments from "../components/comments/Comments";
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
+import NoQuotesFound from "../components/quotes/NoQuotesFound";
+
+const DUMMY_QUOTES = [
+  { id: "q1", author: "Javi", text: "Learning React is fun!" },
+  { id: "q2", author: "Javier", text: "Learning React is great!" },
+];
+
+const QuoteDetail = () => {
+  const match = useRouteMatch();
+  const params = useParams();
+
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+
+  if (!quote) {
+    return <NoQuotesFound />;
+  }
+
+  return (
+    <Fragment>
+      <HighlightedQuote text={quote.text} author={quote.author} />
+      <Route path={match.path} exact>
+        <div className="centered">
+          <Link className="btn--flat" to={`${match.url}/comments`}>
+            Load Comments
+          </Link>
+        </div>
+      </Route>
+      <Route path={`${match.path}/comments`}>
+        <Comments />
+      </Route>
+    </Fragment>
+  );
+};
+
+export default QuoteDetail;
